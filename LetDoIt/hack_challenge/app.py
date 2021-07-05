5f5bbf06faef3f179cc128a675549697ae6b254a
Kelly <1783502751@qq.com>
2021-07-05 16:23:43 +0800

    first

---
+++ LetDoIt/hack_challenge/app.py
@@ -0,0 +1,461 @@
+import json
+import os
+from datetime import datetime
+
+from db import db, User, PublicList, Event, Image, User_PublicList_Association, Item
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+db_filename = "letsdoit.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = False
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+def success_response(data, code=200):
+    return json.dumps({"data": data}), code
+
+
+def failure_response(message, code=404):
+    return json.dumps({"success": False, "error": message}), code
+
+
+def get_user_by_name(name):
+    return User.query.filter_by(name=name).first()
+
+
+def get_user_by_session_token(session_token):
+    return User.query.filter_by(session_token=session_token).first()
+
+
+def get_user_by_update_token(update_token):
+    return User.query.filter_by(update_token=update_token).first()
+
+
+def extract_token(request):
+    auth_header = request.headers.get("Authorization")
+    if auth_header is None:
+        return False, failure_response("missing auth header")
+    bearer_token = auth_header.replace("Bearer ", "").strip()
+    if bearer_token is None or not bearer_token:
+        return False, failure_response("invalid auth header")
+    return True, bearer_token
+
+
+def check_session():
+    success, session_token = extract_token(request)
+    if not success:
+        return False, session_token
+    user = get_user_by_session_token(session_token)
+    if user is None:
+        return False, failure_response("user not found!")
+    if not user.verify_session(session_token):
+        return False, failure_response("invalid session token!")
+    return True, user
+
+
+@app.route("/api/session/", methods=["POST"])
+def update_token():
+    success, update_token = extract_token(request)
+    if not success:
+        return update_token
+    user = get_user_by_update_token(update_token)
+    if user is None:
+        return failure_response("invalid update token: " + update_token)
+    user.renew_session()
+    db.session.commit()
+    return success_response({
+        "session_token": user.session_token,
+        "session_expiration": str(user.session_expiration),
+        "update_token": user.update_token
+    })
+
+
+@app.route("/api/register/", methods=["POST"])
+def register():
+    body = json.loads(request.data.decode())
+    name = body.get('name')
+    if name is None:
+        return failure_response("no name entered")
+    password = body.get('password')
+    if password is None:
+        return failure_response("no password entered")
+    if get_user_by_name(name) is not None:
+        return failure_response("user already exists!")
+    new_user = User(name=name, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return success_response({
+        "session_token": new_user.session_token,
+        "session_expiration": str(new_user.session_expiration),
+        "update_token": new_user.update_token
+    })
+
+
+@app.route("/api/login/", methods=["POST"])
+def login():
+    body = json.loads(request.data.decode())
+    name = body.get('name')
+    if name is None:
+        return failure_response("no name entered")
+    password = body.get('password')
+    if password is None:
+        return failure_response("no password entered")
+    user = get_user_by_name(name)
+    if user is None:
+        return failure_response("user not found!")
+    if not user.verify_password(password):
+        return failure_response("password incorrect!")
+    return success_response({
+        "session_token": user.session_token,
+        "session_expiration": str(user.session_expiration),
+        "update_token": user.update_token
+    })
+
+
+# List routes
+@app.route("/api/lists/")
+def get_all_lists():
+    success, user = check_session()
+    if not success:
+        return user
+    return success_response({"lists": [c.public_list.serialize() for c in user.public_lists if c.is_public]})
+
+
+@app.route("/api/lists/<int:list_id>/")
+def get_list_by_id(list_id):
+    success, user = check_session()
+    if not success:
+        return user
+    public_list = user.public_lists.filter_by(public_list_id=list_id).first()
+    if public_list is None:
+        return failure_response("list not found!")
+    return success_response(public_list.public_list.serialize())
+
+
+@app.route("/api/lists/", methods=['POST'])
+def create_list():
+    body = json.loads(request.data.decode())
+    list_name = body.get('list_name')
+    is_public = body.get('is_public')
+    if is_public is None or list_name is None:
+        return failure_response("Please provide access information/name of list")
+    success, user = check_session()
+    if not success:
+        return user
+    new_list = PublicList(list_name=list_name, publisher_id=user.id)
+    db.session.add(new_list)
+    association = User_PublicList_Association(user_id=user.id, is_public=is_public)
+    association.public_list = new_list
+    user.public_lists.append(association)
+    db.session.add(association)
+    db.session.commit()
+    return success_response(new_list.serialize())
+
+
+@app.route("/api/lists/<int:list_id>/", methods=["POST"])
+def edit_list_by_id(list_id):
+    success, user = check_session()
+    if not success:
+        return user
+    public_list = user.public_lists.filter_by(public_list_id=list_id).first()
+    if public_list is None:
+        return failure_response("list not found!")
+    body = json.loads(request.data.decode())
+    public_list.is_public = body.get('is_public', public_list.is_public)
+    public_list = public_list.public_list
+    public_list.list_name = body.get('list_name', public_list.list_name)
+    db.session.commit()
+    return success_response(public_list.serialize())
+
+
+@app.route("/api/lists/<int:list_id>/delete/", methods=['DELETE'])
+def delete_list_by_id(list_id):
+    success, user = check_session()
+    if not success:
+        return user
+    delete_list = User_PublicList_Association.query.filter_by(user_id=user.id, public_list_id=list_id).first()
+    if delete_list is None:
+        return failure_response("list not found!")
+    public_list = delete_list.public_list
+    db.session.delete(public_list)
+    user.public_lists.remove(delete_list)
+    db.session.delete(delete_list)
+    db.session.commit()
+    return success_response(public_list.serialize())
+
+
+# Event routes
+@app.route("/api/lists/<int:list_id>/events/")
+def get_all_events(list_id):
+    success, user = check_session()
+    if not success:
+        return user
+    public_list = PublicList.query.filter_by(id=list_id).first()
+    if public_list is None:
+        return failure_response('list not found!')
+    return success_response({"events": [c.serialize() for c in public_list.events]})
+
+
+@app.route("/api/lists/<int:list_id>/events/<int:event_id>/")
+def get_event_by_id(list_id, event_id):
+    success, user = check_session()
+    if not success:
+        return user
+    event_list = user.public_lists.filter_by(public_list_id=list_id).first()
+    if event_list is None:
+        return failure_response("list not found!")
+    event_list = event_list.public_list
+    event = event_list.events.filter_by(id=event_id).first()
+    if event is None:
+        return failure_response("event not found!")
+    return success_response(event.serialize())
+
+
+@app.route("/api/lists/<int:list_id>/events/", methods=["POST"])
+def create_event(list_id):
+    success, user = check_session()
+    if not success:
+        return user
+    public_list = PublicList.query.filter_by(id=list_id).first()
+    if public_list is None:
+        return failure_response('list not found!')
+    body = json.loads(request.data.decode())
+    main_title = body.get('main_title')
+    sub_title = body.get('sub_title')
+    in_progress = body.get('in_progress')
+    if main_title is None or sub_title is None or in_progress is None:
+        return failure_response("missing field(s)!")
+    new_event = Event(main_title=main_title, sub_title=sub_title, in_progress=in_progress, public_list_id=list_id)
+    public_list.events.append(new_event)
+    db.session.add(new_event)
+    db.session.commit()
+    return success_response(new_event.serialize(), 201)
+
+
+@app.route("/api/lists/<int:list_id>/events/<int:event_id>/", methods=["POST"])
+def edit_event_by_id(list_id, event_id):
+    success, user = check_session()
+    if not success:
+        return user
+    event_list = user.public_lists.filter_by(public_list_id=list_id).first()
+    if event_list is None:
+        return failure_response("list not found!")
+    event_list = event_list.public_list
+    event = event_list.events.filter_by(id=event_id).first()
+    if event is None:
+        return failure_response("event not found!")
+    body = json.loads(request.data.decode())
+    event.main_title = body.get('main_title', event.main_title)
+    event.sub_title = body.get('sub_title', event.sub_title)
+    event.in_progress = body.get('in_progress', event.in_progress)
+    db.session.commit()
+    return success_response(event.serialize())
+
+
+@app.route("/api/lists/<int:list_id>/events/<int:event_id>/delete/", methods=['DELETE'])
+def delete_event_by_id(list_id, event_id):
+    success, user = check_session()
+    if not success:
+        return user
+    event_list = user.public_lists.filter_by(public_list_id=list_id).first()
+    if event_list is None:
+        return failure_response("list not found!")
+    event_list = event_list.public_list
+    delete_event = event_list.events.filter_by(id=event_id).first()
+    if delete_event is None:
+        return failure_response("event not found!")
+    event_list.events.remove(delete_event)
+    db.session.delete(delete_event)
+    db.session.commit()
+    return success_response(delete_event.serialize())
+
+
+# Friend routes
+@app.route("/api/friends/add/", methods=['POST'])
+def add_friend():
+    success, user = check_session()
+    if not success:
+        return user
+    body = json.loads(request.data.decode())
+    search_id = body.get('id')
+    if search_id is None:
+        return failure_response("no id entered")
+    if id == search_id:
+        return failure_response("cannot send a request to yourself!")
+    search_user = User.query.filter_by(id=search_id).first()
+    if search_user is None:
+        return failure_response("user not found!")
+    already_friend = user.friends.filter_by(id=search_id).first()
+    if already_friend is not None:
+        return failure_response("you already added this user!")
+    already_request = user.applying_friends.filter_by(id=search_id).first()
+    if already_request is not None:
+        return failure_response("you already have a pending response!")
+    search_user.applying_friends.append(user)
+    db.session.commit()
+    return success_response(search_user.serialize())
+
+
+@app.route("/api/friends/accept/<int:friend_id>/", methods=["POST"])
+def accept_friend_request(friend_id):
+    success, user = check_session()
+    if not success:
+        return user
+    friend = User.query.filter_by(id=friend_id).first()
+    if friend is None:
+        return failure_response("friend user not found!")
+    friend = user.applying_friends.filter_by(id=friend_id).first()
+    if friend is None:
+        return failure_response("this user does not have a pending request!")
+    user.friends.append(friend)
+    user.applying_friends.remove(friend)
+    db.session.commit()
+    return success_response(friend.serialize())
+
+
+@app.route("/api/friends/reject/<int:friend_id>/", methods=["POST"])
+def reject_friend_request(friend_id):
+    success, user = check_session()
+    if not success:
+        return user
+    friend = User.query.filter_by(id=friend_id).first()
+    if friend is None:
+        return failure_response("friend user not found!")
+    friend = user.applying_friends.filter_by(id=friend_id).first()
+    if friend is None:
+        return failure_response("this user does not have a pending request!")
+    user.applying_friends.remove(friend)
+    db.session.commit()
+    return success_response(friend.serialize())
+
+
+@app.route("/api/friends/requests/")
+def get_friend_requests():
+    success, user = check_session()
+    if not success:
+        return user
+    return success_response({"requests": [f.serialize() for f in user.applying_friends]})
+
+
+@app.route("/api/friends_lists/")
+def get_friends_lists():
+    success, user = check_session()
+    if not success:
+        return user
+    return success_response({"friends": [f.serialize() for f in user.friends]})
+
+
+# Item routes
+@app.route("/api/lists/<int:list_id>/events/<int:event_id>/items/")
+def get_all_items(list_id, event_id):
+    success, user = check_session()
+    if not success:
+        return user
+    event_list = user.public_lists.filter_by(public_list_id=list_id).first()
+    if event_list is None:
+        return failure_response("list not found!")
+    event_list = event_list.public_list
+    event = event_list.events.filter_by(id=event_id).first()
+    if event is None:
+        return failure_response("event not found!")
+    return success_response({"items": [i.serialize() for i in event.items]})
+
+
+@app.route("/api/lists/<int:list_id>/events/<int:event_id>/items/<int:item_id>/")
+def get_item_by_id(list_id, event_id, item_id):
+    success, user = check_session()
+    if not success:
+        return user
+    event_list = user.public_lists.filter_by(public_list_id=list_id).first()
+    if event_list is None:
+        return failure_response("list not found!")
+    event_list = event_list.public_list
+    event = event_list.events.filter_by(id=event_id).first()
+    if event is None:
+        return failure_response("event not found!")
+    item = event.items.filter_by(id=item_id).first()
+    if item is None:
+        return failure_response("item not found!")
+    return success_response(item.serialize())
+
+
+@app.route("/api/lists/<int:list_id>/events/<int:event_id>/items/", methods=["POST"])
+def create_item(list_id, event_id):
+    success, user = check_session()
+    if not success:
+        return user
+    event_list = user.public_lists.filter_by(public_list_id=list_id).first()
+    if event_list is None:
+        return failure_response("list not found!")
+    event_list = event_list.public_list
+    event = event_list.events.filter_by(id=event_id).first()
+    if event is None:
+        return failure_response("event not found!")
+    body = json.loads(request.data.decode())
+    completed = body.get('completed')
+    date = body.get('date')
+    title = body.get('title')
+    if completed is None or date is None or title is None:
+        return failure_response("missing field(s)!")
+    # datetime.strptime(date, "%Y-%m-%d").date()
+    new_item = Item(completed=completed, date=date, title=title, event_id=event_id)
+    event.items.append(new_item)
+    db.session.add(new_item)
+    db.session.commit()
+    return success_response(new_item.serialize(), 201)
+
+
+@app.route("/api/lists/<int:list_id>/events/<int:event_id>/items/<int:item_id>/", methods=["POST"])
+def edit_item_by_id(list_id, event_id, item_id):
+    success, user = check_session()
+    if not success:
+        return user
+    event_list = user.public_lists.filter_by(public_list_id=list_id).first()
+    if event_list is None:
+        return failure_response("list not found!")
+    event_list = event_list.public_list
+    event = event_list.events.filter_by(id=event_id).first()
+    if event is None:
+        return failure_response("event not found!")
+    item = event.items.filter_by(id=item_id).first()
+    if item is None:
+        return failure_response("item not found!")
+    body = json.loads(request.data.decode())
+    item.completed = body.get('completed', item.completed)
+    item.date = body.get('date', item.date)
+    item.title = body.get('title', item.title)
+    db.session.commit()
+    return success_response(item.serialize())
+
+
+@app.route("/api/lists/<int:list_id>/events/<int:event_id>/items/<int:item_id>/delete/", methods=['DELETE'])
+def delete_item_by_id(list_id, event_id, item_id):
+    success, user = check_session()
+    if not success:
+        return user
+    event_list = user.public_lists.filter_by(public_list_id=list_id).first()
+    if event_list is None:
+        return failure_response("list not found!")
+    event_list = event_list.public_list
+    event = event_list.events.filter_by(id=event_id).first()
+    if event is None:
+        return failure_response("event not found!")
+    delete_item = event.items.filter_by(id=item_id).first()
+    if delete_item is None:
+        return failure_response("item not found!")
+    event.items.remove(delete_item)
+    db.session.delete(delete_item)
+    db.session.commit()
+    return success_response(delete_item.serialize())
+
+
+if __name__ == "__main__":
+    port = os.environ.get('PORT', 5000)
+    app.run(host="0.0.0.0", port=port)
---
+++ LetDoIt/hack_challenge/db.py
@@ -0,0 +1,188 @@
+import datetime
+import hashlib
+import os
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+friends_association = db.Table(
+    'friends',
+    db.Column('user_id1', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('user_id2', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
+
+applying_friends_association = db.Table(
+    'friends_apply',
+    db.Column('user_id1', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('user_id2', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
+
+list_events_association_table = db.Table(
+    'list_events_association',
+    db.Column('list_id', db.Integer, db.ForeignKey('public_list.id')),
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'))
+)
+
+event_items_association_table = db.Table(
+    'event_items_association',
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id')),
+    db.Column('item_id', db.Integer, db.ForeignKey('item.id'))
+)
+
+
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    public_lists = db.relationship("User_PublicList_Association", back_populates="user", lazy='dynamic')
+    session_token = db.Column(db.String, nullable=False, unique=True)
+    session_expiration = db.Column(db.DateTime, nullable=False)
+    update_token = db.Column(db.String, nullable=False, unique=True)
+
+    applying_friends = db.relationship(
+        'User',
+        secondary=applying_friends_association,
+        primaryjoin=(applying_friends_association.c.user_id1 == id),
+        secondaryjoin=(applying_friends_association.c.user_id2 == id),
+        lazy='dynamic',
+        backref=db.backref('applying_friends_association', lazy='dynamic')
+    )
+
+    friends = db.relationship(
+        'User',
+        secondary=friends_association,
+        primaryjoin=(friends_association.c.user_id1 == id),
+        secondaryjoin=(friends_association.c.user_id2 == id),
+        lazy='dynamic',
+        backref=db.backref('friends_association', lazy='dynamic')
+    )
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+        salt = os.urandom(32)
+        self.password = salt + hashlib.pbkdf2_hmac('sha256', kwargs.get('password').encode('utf-8'), salt, 100000)
+        self.renew_session()
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "friends": [f.serialize() for f in self.friends],
+            "public_list": [pl.serialize() for pl in self.public_lists]
+        }
+
+    def verify_password(self, password):
+        salt = self.password[:32]
+        return self.password[32:] == hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+
+    def _urlsafe_base_64(self):
+        return hashlib.sha1(os.urandom(64)).hexdigest()
+
+    def renew_session(self):
+        self.session_token = self._urlsafe_base_64()
+        self.session_expiration = datetime.datetime.now() + datetime.timedelta(days=1.5)
+        self.update_token = self._urlsafe_base_64()
+
+    def verify_session(self, session_token):
+        return session_token == self.session_token and datetime.datetime.now() < self.session_expiration
+
+    def verify_update_token(self, update_token):
+        return update_token == self.update_token
+
+
+class PublicList(db.Model):
+    __tablename__ = "public_list"
+    id = db.Column(db.Integer, primary_key=True)
+    publisher_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    list_name = db.Column(db.String, nullable=False)
+    events = db.relationship("Event", cascade="delete", lazy='dynamic')
+    users = db.relationship("User_PublicList_Association", back_populates="public_list", lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        self.list_name = kwargs.get('list_name')
+        self.publisher_id = kwargs.get('publisher_id')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "list_name": self.list_name,
+            "events": [e.serialize() for e in self.events]
+        }
+
+
+# https://docs.sqlalchemy.org/en/14/orm/basic_relationships.html#association-object
+class User_PublicList_Association(db.Model):
+    __tablename__ = 'user_publiclist_association'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    public_list_id = db.Column(db.Integer, db.ForeignKey('public_list.id'), primary_key=True)
+    is_public = db.Column(db.Boolean, nullable=False)
+    user = db.relationship("User", back_populates="public_lists")
+    public_list = db.relationship("PublicList", back_populates="users")
+
+    def __init__(self, **kwargs):
+        self.user_id = kwargs.get('user_id')
+        self.is_public = kwargs.get('is_public')
+
+
+class Event(db.Model):
+    __tablename__ = "event"
+    id = db.Column(db.Integer, primary_key=True)
+    main_title = db.Column(db.String, nullable=False)
+    sub_title = db.Column(db.String, nullable=True)
+    in_progress = db.Column(db.Boolean, nullable=False)
+    public_list_id = db.Column(db.Integer, db.ForeignKey('public_list.id'))
+    items = db.relationship('Item', back_populates="event", cascade='delete', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        self.main_title = kwargs.get('main_title')
+        self.sub_title = kwargs.get('sub_title')
+        self.in_progress = kwargs.get('in_progress')
+        self.public_list_id = kwargs.get('public_list_id')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "main_title": self.main_title,
+            "sub_title": self.sub_title,
+            "in_progress": self.in_progress,
+            "items": [i.serialize() for i in self.items]
+        }
+
+    def item_serialize(self):
+        return {
+            "id": self.id,
+            "main_title": self.main_title,
+            "sub_title": self.sub_title,
+            "in_progress": self.in_progress
+        }
+
+
+class Item(db.Model):
+    __tablename__ = "item"
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
+    completed = db.Column(db.Boolean, nullable=False)
+    date = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    event = db.relationship("Event", back_populates="items")
+
+    def __init__(self, **kwargs):
+        self.completed = kwargs.get('completed')
+        self.date = kwargs.get('date')
+        self.title = kwargs.get('title')
+        self.event_id = kwargs.get('event_id')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "completed": self.completed,
+            "date": self.date,
+            "title": self.title
+        }
+
+
+class Image(db.Model):
+    __tablename__ = "images"
+    id = db.Column(db.Integer, primary_key=True)
